34d1bbc538a263628672a2270d4b9d0fd2f36893
Matt Mont <mattmont415@gmail.com>
2021-01-10 18:03:13 -0800

    Completed the Soul Number extended information

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ let objLetterNumber = {
   S: 1, T: 2, U: 3, V: 4, W: 5, X: 6, Y: 7, Z: 8, '7': 7
 }
 
+//Passed from the HTML file to correctly format the printed data
 function returnString() {
   //Get the name and date
   let fullname = document.getElementById("nametext").value;
@@ -15,20 +16,22 @@ function returnString() {
   let vowels = fullname.split("").filter(x => vowelStr.indexOf(x) > -1).join("");
   let conson = fullname.split("").filter(x => consoStr.indexOf(x) > -1).join("");
 
+  //Fills out the entered information, followed by detailed number descriptions
   document.getElementById("textdisplay").innerHTML = fullname;
   document.getElementById("bdaydisplay").innerHTML = translateDate(date);
-    document.getElementById("lifenumberinfo").innerHTML = getLifeNumberInfo(calcWordNumber(date));
+
+  //Life Number Printing - Both upper and lower
   document.getElementById("lifelessonnum").innerHTML = calcWordNumber(date);
+    document.getElementById("lifenumberinfo").innerHTML = getLifeNumberInfo(calcWordNumber(date));
+  //Soul Number Printing - Both upper and lower
   document.getElementById("soulnum").innerHTML = calcWordNumber(vowels);
+    document.getElementById("soulnumberinfo").innerHTML = getSoulNumberInfo(calcWordNumber(vowels))
+  //Outer Personality Number Printing 
   document.getElementById("outernum").innerHTML = calcWordNumber(conson);
+
+  //Destiny Number Printing
   document.getElementById("destinynum").innerHTML = calcWordNumber(fullname);
 
-  //In Calc word number, just get the ['Left/Master' and 'Left/Base']
-
-  //Have a separate function to format it to "Master number: " for the purpose of the strings
-
-  //Click here to get a detailed explanation of what your numbers mean!
-  //Try to pass all the numbers to the link so that they can be read from the text file
 }
 
 function translateDate(strDate) {
@@ -45,7 +48,8 @@ function translateDate(strDate) {
   return month + " " + day + ", " + year;
 }
 
-//*** COME BACK HERE!!!
+
+//This will print the life number info based on the calculation
 function getLifeNumberInfo(nums) {
   //Input will be as an array, either length 2 (has master) or length 1, w/ no master
   let strReturn = `This represents the lessons you must learn in this lifetime and is most significant in your choice of a career. It is derived from your full birthdate. Even a business or idea has a moment of "birth" and can therefore be treated in the same way. The Life Lesson Number is a constant and cannot be changed or modified in any way because it is derived from the birthdate. It indicates vocation plus the lesson you came into incarnation to overcome or to learn. It also represents the cosmic gift you are given in order to accomplish your destiny.<br><br>`;
@@ -72,6 +76,7 @@ function getLifeNumberInfo(nums) {
   let leftNum = 0;
   let masterNum = 0;
   let breakNum = [];
+
   //If no master, split the numbers;
   if (nums.length === 1) {
     breakNum = nums[0].split("/");
@@ -88,16 +93,62 @@ function getLifeNumberInfo(nums) {
     if (masterNum === 22) strReturn += lifeNumStats[10];
     if (masterNum === 33) strReturn += lifeNumStats[11];
     if (masterNum === 44) strReturn += lifeNumStats[12];
-    if (baseNum < 10) strReturn += "<br><br>Also the base number..." + lifeNumStats[baseNum-1];
+    if (baseNum < 10) strReturn += "<br><br>Also the base number can provide helpful knowledge...<br>" + lifeNumStats[baseNum-1];
   }
   //Display the Life Lesson Number Label formatted to be red
   document.getElementById("lbllifelessonlower").innerHTML = "Life Lesson Number: "
   return strReturn;
 }
 
-function readText(filePath) {
-  let outText = "";
+
+
+function getSoulNumberInfo(nums) {
+  let strReturn = `Your Soul Number is your real personality, the you that is known only to you. If one embraces the philosophy of reincarnation, the Soul Number also indicates what you have been in previous lifetimes. This part of your personality is not easily recognized by others unless they know you very well. The Soul Number is what you, in your in- ner secret self, desire to be. This urge may be so strong that it can overcome other vibrations of your four basic numbers. Since the Soul Number reveals something of accumulated growth in past lives, it becomes an underlying force which influences the actions of your present life. If, however, the soul urge remains repressed by outer circumstances, and the soul fails to accomplish its purpose, it may need to repeat the same vibrational urge in a future life, until it finds true expression.<br><br>`;
+
+  let soulNumStats = [
+    `***1*** THE SOUL NUMBER: The leadership won in past lives now brings a desire to continue striving for higher consciousness. You are independent regarding your beliefs. Your desire for free and independent thinking continues to occupy your inner-most yearning. Do not let this strong drive obstruct your attainmentof practical goals in the present life. You are always conscious of your inner strength and would have difficulty taking a secondary position among your contemporaries. If marriage or partnership is considered, investigate the inner yearnings of your prospective partner to safeguard the successful outcome of your relationship. If your individuality is too strong, it could express itself as bossiness and be a detriment to personal happiness. So firm is the intensity of your focus and so strong the memory of self-glory from the past that you are apt to stand on your convictions even though it could disrupt important relationships. Your inner strength gives you something to rely on when the going is rough, and you can be a tower of inspiration to others in times of trouble.******`,
+    `***2*** THE SOUL NUMBER: You have a strong desire for peace and harmony. You are considerate and tactful, adaptable and gentle. You are a follower rather than a leader. Tact is a strong point in your makeup; therefore, you are a go-between or agent, helping to bring peace between opposing forces. You avoid hurting the feelings of others to the point of subordinating yourself to their wills. As a I result, you appear shy and lacking in confidence. Try to overcome indecision; while you are hesitating, others may forge ahead of you and claim what should be yours. Dare to do what you know is right and do not let emotions deter your purpose. Your sensitivity can be positive when used to tune in to the balancing forces of the universe and bring forth truths which help all people gain understanding.******`,
+    `***3*** THE SOUL NUMBER: You are very conscientious in regard to duty. You are well aware of the law of the Trinity, and know that inspiration and imagination will bring the best results when used to help others. This could easily become your philosophy of life. Follow your urge to create and expand the activities that interest you. You seek happiness and find it in making others happy. If a person feels depressed, a visit with you will bring them hope and courage. Expand your ideals by dedicating yourself to the expression of good cheer and optimism. Work to make your dreams come true, but not to the extreme of becoming impractical. Love is important to you — both in giving and receiving — but try to hold to reason in your loving expansiveness. You become happy and well-adjusted by making others happy.******`,
+    `***4*** THE SOUL NUMBER: You are well organized, a trait which could bring you material success. Your practicality permeates your entire being. Others could set a pattern after your well-ordered program of living. Your loyalty, balance and dependability mean much to those around you. They know where you stand in all things and feel that any dealings with you will be handled squarely. You take matters seriously, whether in business or romance, and can therefore make your dreams come true in a planned, practical way.******`,
+    `***5*** THE SOUL NUMBER: You claim the right to freedom and will not allow any limitations of your ideals or ways of thinking. Variety of self-expression is absolutely essential. You would feel dull and listless without the stimulation of changes and new outlooks. Travel is one of your soul's desires since you believe it to be educational and broadening. Narrowness cannot be tolerated. If you feel yourself falling into a rut, a trip, a new outfit or a vacation could change the vibrations for the better and open new avenues for continued inner growth.******`,
+    `***6*** THE SOUL NUMBER: You respond to beauty, harmony and peace. You are affectionate, sympathetic and loyal to those you love. Your mission could be to teach others to maintain peace and harmony in their lives and to spread the idea of the Golden Rule. You work hard to keep domestic harmony as your ideal way of life. 24 Of all the love vibrations in the various numbers, yours is the most likely to be guilty of a smothering love, so deep is your desire to live for your own immediate family. Learn to allow your family members to express their own desires in life, even if you do not agree with their choices.******`,
+    `***7*** THE SOUL NUMBER: You are quiet and reserved, a good thinker, analyzer and mediator. You need peaceful surroundings and become irritable if your environment is noisy. You are re- fined, sensitive, secretive and usually psychic. You may live alone and remain unmarried. You could be celibate and join the higher mystical order of humanity. Your true nature is to be calm, to develop depth of character, and thus to benefit humanity through philosophy.******`,
+    `***8*** THE SOUL NUMBER: Ambition is your keyword. You believe in accomplishment and let no obstacle deter you in attaining your goal. Your number is not an easy one to handle, but the rewards are worth your effort. You are one who will tackle a big job in order to rise above the crowd and arrive at the pinnacle. You have the ability to organize large groups and undertakings successfully. Psychology will help you understand the masses with whom you may work. Others expect more of you than of the average person, so you must rely upon your inner self to guide you to stay at the top. This is the number of high-ranking sports figures.******`,
+    `***9*** THE SOUL NUMBER: Intuition is strongly active in your life. You are sensitive and imaginative and can think in abstract terms. Although you may appear vague at times, you are ex- tremely impressionable, compassionate and generous. You need to have and to give love. You are kind and forgiving, with an expansive consciousness dedicated to uplifting humanity. This could be the number of a master or adept from a previous lifetime.******`,
+    `***11*** THE SOUL NUMBER: You have been on the spiritual path for a long time, probably for more than one incarnation. Through spiritual evolution you have learned much of the mysteries of life and death. You have courage, talent and leadership ability. You are understanding, wise, intuitive and often clairvoyant, with extremely sensitive ESP abilities and strong spiritual leanings. You also have the fortitude to contend with many changes and unexpected events.******`,
+    `***22*** THE SOUL NUMBER: With the power of 22, you have the urge to continue the tangible achievements of past lifetimes. You desire material fulfillment. You are the master builder. Your soul's aim is to leave the world a tangibly better place for your having been here, so it follows that you must keep mental balance while expressing your ideals in practical ways. You have higher goals than the average person; you should keep your feet on the ground while your thoughts expand.******`,
+    `***33*** THE SOUL NUMBER: You are ready to sacrifice for humanity. You see clearly a vision of future world conditions and feel ready to help in any way you can to bring peace to all mankind. Sometimes your life lesson vibrations can be in opposition to your soul's urges; nevertheless, you will practice generosity and try to see other people's points of view. This circumstance may force you temporarily to take a background position but the hope remains that you will reach a position to promote your ideals. You stand ever ready to help others.******`,
+    `***44*** THE SOUL NUMBER: Universal concepts are part of your consciousness; they now express themselves as inner urges to accomplish great advances in world culture. You desire to unite the practical with the philosophical. A career within the government or in the United Nations could give you the opportunity to promote these ideals among all peoples. You are willing to shoulder heavy responsibilities. Your inner self realizes that it is adept; now the problem becomes how to manifest it in the outer world. You believe that your soul will guide you into fields where your great expectations can be promoted. You have an innate ability to solve everyday problems and can work with others to help them organize their lives.******`
+  ];
+
+  let baseNum = 0;
+  let leftNum = 0;
+  let masterNum = 0;
+  let breakNum = [];
+  
+  if (nums.length === 1) {
+    breakNum = nums[0].split("/");
+    leftNum = parseInt(breakNum[0]);
+    baseNum = parseInt(breakNum[1]);
+    if (baseNum < 10) strReturn += soulNumStats[baseNum-1];
+  } else {
+    //Case of a master num, add the master description AND the base num script
+    breakNum = nums[0].split("/");
+    leftNum = parseInt(breakNum[0]);
+    masterNum = parseInt(breakNum[1]);
+    baseNum = parseInt(nums[1]);
+    if (masterNum === 11) strReturn += soulNumStats[9];
+    if (masterNum === 22) strReturn += soulNumStats[10];
+    if (masterNum === 33) strReturn += soulNumStats[11];
+    if (masterNum === 44) strReturn += soulNumStats[12];
+    if (baseNum < 10) strReturn += "<br><br>Also the base number can provide useful knowledge...<br>" + soulNumStats[baseNum-1];
+  }
+  //Display the Life Lesson Number Label formatted to be red
+  document.getElementById("lblsoullessonlower").innerHTML = "Soul Number: "
+  return strReturn;
 }
+
+
 
 function calcWordNumber(string) {
 	string = string.toUpperCase();
